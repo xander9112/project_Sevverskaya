0946fe0a62c4da31917db2bff6b7417abca9b599
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2020-10-13 19:05:10 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.scss";
+import { NewComponent } from "./components/NewComponent";
+
+function App() {
+  return (
+    <div className="App">
+      App
+      <NewComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./style.scss";
+
+export const NewComponent: React.FC = () => {
+  return <div className="layooo">layooo</div>;
+};
